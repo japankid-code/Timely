33877254d405e819e5c5b9354a7e7f4d0ea36d92
jake rankin <japankid.jake@gmail.com>
2021-10-31 11:04:34 -0500

    add android wear project files

---
+++ app/src/main/java/com/example/timely/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.timely
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.timely.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
