d0d6b4ac63d84c876e035a10d3c88fe8f042c804
jake rankin <japankid.jake@gmail.com>
2021-11-11 22:25:26 -0600

    add ids, icons and connect activity to ui

--- app/src/main/java/com/example/timely/MainActivity.kt
+++ app/src/main/java/com/example/timely/MainActivity.kt
@@ -2,17 +2,31 @@ package com.example.timely
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import com.example.timely.databinding.ActivityTimerBinding
 
-public class MainActivity : Activity()  {
+class MainActivity : Activity()  {
 
+    private var timer = Timer(300.0)
     private lateinit var binding: ActivityTimerBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        binding = ActivityTimerBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
-    }
+            binding = ActivityTimerBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            val btnPlayPause = findViewById<ImageButton>(R.id.btn_play_pause)
+            val secondsLeft = findViewById<TextView>(R.id.seconds_left)
+            val minutesLeft = findViewById<TextView>(R.id.minutes_left)
+            btnPlayPause.setOnClickListener {
+                Toast.makeText(this@MainActivity, "hehe.", Toast.LENGTH_SHORT).show()
+                timer.timerHandler("start")
+                secondsLeft.text = String.format("%02d", timer.secondsLeft)
+                minutesLeft.text = String.format("%02d", timer.minutesLeft)
+            }
+        }
 }
--- app/src/main/java/com/example/timely/Timer.kt
+++ app/src/main/java/com/example/timely/Timer.kt
@@ -1,17 +1,27 @@
 package com.example.timely
+import android.app.Activity
 import android.os.CountDownTimer
 
-class Timer(private var seconds: Long) {
+class Timer(private var s: Double = 0.0) : Activity() {
     // properties
-    private var secondsLeft: Long = seconds
+    private var seconds = s
+    var secondsLeft: Int = (seconds % 60).toInt()
+    var minutesLeft: Int = kotlin.math.floor(seconds/60).toInt()
     private var isStarted: Boolean = false
     private var isPaused: Boolean = false
 
+
+    fun setSecondsLeftTo(seconds: Int) {
+        secondsLeft = seconds
+    }
+
     fun timerHandler(opt: String) {
-        val millisecondsLeft = seconds * 1000
+        val millisecondsLeft = seconds.toLong() * 1000
         val timer = object: CountDownTimer(millisecondsLeft, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                secondsLeft = millisUntilFinished/1000
+            override fun onTick(msTilFinished: Long) {
+                seconds = (msTilFinished.toDouble()/1000)
+                setSecondsLeftTo(kotlin.math.ceil((msTilFinished.toDouble()/1000) % 60).toInt())
+                minutesLeft = kotlin.math.floor(seconds/60).toInt()
             }
             override fun onFinish() {
                 // add alert calls here
@@ -31,11 +41,12 @@ class Timer(private var seconds: Long) {
             "reset" -> {
                 isStarted = false
                 isPaused = true
-                secondsLeft = seconds
+                seconds = s
             }
             else -> {
                 print("no timer option selected")
             }
         }
     }
+
 }
